16d5c7e59425a3303ef94e06f534d7363e888ad9
tianxia.jia <tianxia.jia@ucalgary.ca>
2024-03-26 17:02:03 -0600

    update news

--- misc/update_NEWS.R
+++ misc/update_NEWS.R
@@ -42,3 +42,36 @@ my_news$add_bullet(c("misc/update_NEWS.R"))
 
 my_news$get_text()
 my_news$write()
+
+#------------------------------------------------------------------------------#
+# version 1.1.0
+#------------------------------------------------------------------------------#
+
+my_news$add_version("1.1.0")
+
+my_news$add_subtitle("New features")
+my_news$add_bullet(c("Kriging for new locations are supported",
+                     "Exponential Lagrangian correlation function added"))
+
+my_news$add_subtitle("Bug fixes")
+my_news$add_bullet(c("`find_dists`: fixed bug for computing long/lat distnaces",
+                     "`add_lagr`: `dists_base` -> `dists_lagr`"))
+
+my_news$add_subtitle("Function updates")
+my_news$add_bullet(c("added two arguments to mcgf()"))
+my_news$add_bullet(c("added exponential function for the Lagrangian model"))
+
+my_news$add_subtitle("New functions")
+my_news$add_bullet(c("R/cor_lagr_exp.R"))
+my_news$add_bullet(c("R/find_dists_new.R"))
+my_news$add_bullet(c("R/krige_new.R"))
+
+my_news$add_subtitle("Miscellaneous")
+my_news$add_bullet(c(
+    "Updated simulated samples throughout the package to allow new locations"
+))
+my_news$add_bullet(c("Updated description and modified reference"))
+my_news$add_bullet(c("New vignettes for regime-switching models"))
+
+my_news$get_text()
+my_news$write()
